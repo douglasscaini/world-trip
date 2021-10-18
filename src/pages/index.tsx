ec3e52a87badb79d780b8a72f898386a044f16ab
Douglas Scaini <douglasscaini.dev@gmail.com>
2021-10-18 17:45:39 -0300

    feat: Config Prismic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,19 @@ import Banner from "../components/Banner";
 import TravelTypes from "../components/TravelTypes";
 import Divider from "../components/Divider";
 import SlideSwiper from "../components/SlideSwiper";
+import { GetStaticProps } from "next";
+import { getPrismicClient } from "../services/prismic";
+import Prismic from "@prismicio/client";
 
-export default function Home() {
+type Continent = {
+  slug: string;
+};
+
+type ContinentsProps = {
+  continents: Continent[];
+};
+
+export default function Home({ continents }: ContinentsProps) {
   return (
     <Flex w="100%" direction="column" overflowX="hidden">
       <Header />
@@ -15,7 +26,7 @@ export default function Home() {
       <Center flexDirection="column">
         <Divider />
         <Text mt={["6", "14"]} fontSize={["xl", "4xl"]} fontWeight="500" color="gray.450">
-          Vamos Nessa?
+          Vamos Nessa? {}
         </Text>
         <Text fontSize={["xl", "4xl"]} fontWeight="500" color="gray.450">
           Então escolha seu continente
@@ -26,3 +37,17 @@ export default function Home() {
     </Flex>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query(Prismic.predicates.at("document.type", "continent"));
+
+  const continents = response.results.map((continent) => continent.data);
+
+  console.log(continents);
+
+  return {
+    props: { continents },
+  };
+};
